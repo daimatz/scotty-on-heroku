50933efadee595dca08b44fcdbbff5e679e67f56
daimatz <dai@daimatz.net>
2013-02-09 04:37:45 +0900

    complete for heroku

--- src/DBUtil.hs
+++ src/DBUtil.hs
@@ -8,10 +8,31 @@ import Database.Persist.GenericSql (SqlPersist(..), runSqlConn)
 
 import Model
 
--- #define DEVELOPMENT
+-- #define SQLite
+#define HEROKU
 
-#ifdef DEVELOPMENT
+#ifdef HEROKU
 
+import Data.Monoid ((<>))
+import Data.Text.Encoding (encodeUtf8)
+import Database.Persist.Postgresql (withPostgresqlConn)
+import Database.Persist.Store (loadConfig, applyEnv)
+import Web.Heroku (dbConnParams)
+
+runSql :: SqlPersist IO a -> IO a
+runSql query = do
+    params <- dbConnParams
+    let connStr = foldr (\(k,v) t -> t <> (encodeUtf8 $ k <> "=" <> v <> " ")) "" params
+    withPostgresqlConn connStr $ runSqlConn query
+
+-- HEROKU
+#else
+
+-- work on localhost
+
+#ifdef SQLite
+
+-- use SQLite on localhost
 import Database.Persist.Sqlite (withSqliteConn)
 
 runSql :: SqlPersist IO a -> IO a
@@ -19,8 +40,10 @@ runSql = withSqliteConn dbfile . runSqlConn
   where
     dbfile = "db.sqlite3"
 
+-- SQLite
 #else
 
+-- use PostgreSQL on localhost
 import Database.Persist.Postgresql (withPostgresqlConn)
 
 runSql :: SqlPersist IO a -> IO a
@@ -28,6 +51,10 @@ runSql = withPostgresqlConn connStr . runSqlConn
   where
     connStr = "host=localhost port=5432 user=daimatz dbname=daimatz password=daimatz"
 
+-- SQLite
+#endif
+
+-- HEROKU
 #endif
 
 readPosts :: IO [Post]
