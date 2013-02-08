8614b211e4992050af8eebe31812a729030caf3a
daimatz <dai@daimatz.net>
2013-02-09 05:43:54 +0900

    simplify DBUtil.hs (pg or sqlite3)

--- src/DBUtil.hs
+++ src/DBUtil.hs
@@ -9,9 +9,17 @@ import Database.Persist.GenericSql (SqlPersist(..), runSqlConn)
 import Model
 
 -- #define SQLite
-#define HEROKU
 
-#ifdef HEROKU
+#ifdef SQLite
+
+import Database.Persist.Sqlite (withSqliteConn)
+
+runSql :: SqlPersist IO a -> IO a
+runSql = withSqliteConn dbfile . runSqlConn
+  where
+    dbfile = "db.sqlite3"
+
+#else
 
 import Data.Monoid ((<>))
 import Data.Text.Encoding (encodeUtf8)
@@ -25,36 +33,6 @@ runSql query = do
     let connStr = foldr (\(k,v) t -> t <> (encodeUtf8 $ k <> "=" <> v <> " ")) "" params
     withPostgresqlConn connStr $ runSqlConn query
 
--- HEROKU
-#else
-
--- work on localhost
-
-#ifdef SQLite
-
--- use SQLite on localhost
-import Database.Persist.Sqlite (withSqliteConn)
-
-runSql :: SqlPersist IO a -> IO a
-runSql = withSqliteConn dbfile . runSqlConn
-  where
-    dbfile = "db.sqlite3"
-
--- SQLite
-#else
-
--- use PostgreSQL on localhost
-import Database.Persist.Postgresql (withPostgresqlConn)
-
-runSql :: SqlPersist IO a -> IO a
-runSql = withPostgresqlConn connStr . runSqlConn
-  where
-    connStr = "host=localhost port=5432 user=daimatz dbname=daimatz password=daimatz"
-
--- SQLite
-#endif
-
--- HEROKU
 #endif
 
 readPosts :: IO [Post]
