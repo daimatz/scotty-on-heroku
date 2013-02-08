75f56f5477db3515d0aef2413cf462cfe8821b2b
daimatz <dai@daimatz.net>
2013-02-09 04:09:53 +0900

    complete for postgresql

--- src/DBUtil.hs
+++ src/DBUtil.hs
@@ -8,12 +8,27 @@ import Database.Persist.GenericSql (SqlPersist(..), runSqlConn)
 
 import Model
 
+-- #define DEVELOPMENT
+
+#ifdef DEVELOPMENT
+
 import Database.Persist.Sqlite (withSqliteConn)
 
 runSql :: SqlPersist IO a -> IO a
 runSql = withSqliteConn dbfile . runSqlConn
   where
-      dbfile = "db.sqlite3"
+    dbfile = "db.sqlite3"
+
+#else
+
+import Database.Persist.Postgresql (withPostgresqlConn)
+
+runSql :: SqlPersist IO a -> IO a
+runSql = withPostgresqlConn connStr . runSqlConn
+  where
+    connStr = "host=localhost port=5432 user=daimatz dbname=daimatz password=daimatz"
+
+#endif
 
 readPosts :: IO [Post]
 readPosts = map entityVal <$> (runSql $ selectList [] [Desc PostId])
