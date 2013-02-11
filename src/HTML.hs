21329686c1603e3c0b5d2b9aebcebed948f00060
daimatz <dai@daimatz.net>
2013-02-11 14:32:19 +0900

    use loop syntax in mustache

--- src/HTML.hs
+++ src/HTML.hs
@@ -1,12 +1,13 @@
+{-# LANGUAGE RankNTypes #-}
+
 module HTML
     ( formHTML
     , postsHTML
     , body
     ) where
 
-import           Control.Monad           (forM)
 import           Control.Monad.IO.Class  (liftIO, MonadIO)
-import           Data.Monoid             ((<>), mconcat)
+import           Data.Monoid             ((<>))
 import qualified Data.Text               as T
 import           Data.Text.Lazy          (Text)
 import           Data.Text.Lazy.Encoding (decodeUtf8)
@@ -18,27 +19,29 @@ import           Web.Scotty              (html, ActionM)
 
 import Model
 
-mustache :: MonadIO m => FilePath -> (String -> MuType m) -> m Text
+mustache :: MonadIO m => FilePath -> MuContext m -> m Text
 mustache path context = return . decodeUtf8
-    =<< hastacheFile defaultConfig path (mkStrContext context)
+    =<< hastacheFile defaultConfig path context
 
-nullContext :: MonadIO m => String -> MuType m
-nullContext = const $ MuVariable ("" :: String)
+nullContext :: MonadIO m => MuContext m
+nullContext = mkStrContext $ const $ MuVariable ("" :: T.Text)
+
+errorRef :: forall m . MuType m
+errorRef = MuVariable ("error" :: T.Text)
 
 formHTML :: ActionM Text
 formHTML = liftIO $ mustache "view/form.mustache" nullContext
 
 postsHTML :: [Post] -> ActionM Text
-postsHTML posts = do
-    htmls <- forM posts $ \post ->
-        liftIO $ mustache "view/post.mustache" (context post)
-    return $ "<ul>" <> mconcat htmls <> "</ul>"
+postsHTML posts = liftIO $ mustache "view/post.mustache" (mkStrContext context)
   where
-    context post "name" = MuVariable $ postName post
-    context post "text" = MuVariable $ postText post
-    context post "date" = MuVariable
+    context "posts" = MuList $ map (mkStrContext . listContext) posts
+    context _       = errorRef
+    listContext post "name" = MuVariable $ postName post
+    listContext post "text" = MuVariable $ postText post
+    listContext post "date" = MuVariable
         $ T.pack $ formatTime defaultTimeLocale "%F %T" $ postCreated post
-    context _    _      = MuVariable $ ("error" :: T.Text)
+    listContext _    _      = errorRef
 
 body :: Text -> ActionM ()
 body text = do
